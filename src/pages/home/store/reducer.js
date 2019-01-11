aa1a92cfb359f0074c6df747c6444005e701e831
LingHS <a4643895@163.com>
2019-01-11 21:37:10 +0800

    home组件reducer设计

---
+++ src/pages/home/store/reducer.js
@@ -0,0 +1,41 @@
+import { fromJS } from 'immutable';
+// import * as constants from './constatnts';
+
+const defaultState = fromJS({
+  topicList: [{
+    id: 1,
+    title: '读书',
+    imgUrl: '//upload.jianshu.io/collections/images/4/sy_20091020135145113016.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }, {
+    id: 2,
+    title: '手绘',
+    imgUrl: '//upload.jianshu.io/collections/images/283250/%E6%BC%AB%E7%94%BB%E4%B8%93%E9%A2%98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }, {
+    id: 3,
+    title: '自然科普',
+    imgUrl: '//upload.jianshu.io/collections/images/76/12.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }, {
+    id: 4,
+    title: '简书电影',
+    imgUrl: '//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }, {
+    id: 5,
+    title: '摄影',
+    imgUrl: '//upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }, {
+    id: 6,
+    title: '故事',
+    imgUrl: '//upload.jianshu.io/collections/images/95/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }, {
+    id: 7,
+    title: '@IT·互联网',
+    imgUrl: '//upload.jianshu.io/collections/images/14/6249340_194140034135_2.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+  }],
+});
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -5,7 +5,7 @@ overflow:hidden;
 width: 960px;
 margin: 0 auto;
 // height: 300px;
-background: red;
+// background: red;
 `
 ;
 export const HomeLeft = style.div`
@@ -24,4 +24,30 @@ width: 240px;
 float: right;
 `
 ;
-
+export const TopicWrapper = style.div`
+padding: 20px 0 10px 0;
+overflow:hidden;
+margin-left:-18px;
+`
+;
+export const TopicItem = style.div`
+float:left;
+background:#f7f7f7;
+height:32px;
+line-height:32px;
+font-size:14px;
+color:#000;
+border: 1px solid #dcdcdc;
+border-radius:4px;
+margin-left: 18px;
+padding-right: 10px;
+margin-bottom: 18px;
+.topic-pic{
+    width:32px;
+    height:32px;
+    float:left;
+    display:block;
+    margin-right: 10px;
+}
+`
+;
